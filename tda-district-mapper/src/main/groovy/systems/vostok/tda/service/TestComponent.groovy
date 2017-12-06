75f7c9c81d6889ee1e57c4d06dbd0b21ab91a9ca
MarkKlimenko <22773@bk.ru>
2017-12-06 21:19:43 +1000

    Initial commit

---
+++ tda-district-mapper/src/main/groovy/systems/vostok/tda/service/TestComponent.groovy
@@ -0,0 +1,62 @@
+package systems.vostok.tda.service
+
+class TestComponent {
+    static List mapperSimple = [
+            [streetId: 'Svetlanskaya' , building:'1-24' , districtId: 'Center' ],
+            [streetId: 'Svetlanskaya' , building:'25-50' , districtId: 'Dalzavod' ],
+            [streetId: 'Svetlanskaya' , building:'51-106' , districtId: 'Lugovaya' ]
+    ]
+
+    static List mapperLiteral = [
+            [streetId: 'Svetlanskaya' , building:'1-24В' , districtId: 'Center' ],
+            [streetId: 'Svetlanskaya' , building:'24В-24З' , districtId: 'Dalzavod' ],
+            [streetId: 'Svetlanskaya' , building:'24З-106' , districtId: 'Lugovaya' ]
+    ]
+
+    static List mapperDash = [
+            [streetId: 'Svetlanskaya' , building:'1-24В' , districtId: 'Center' ],
+            [streetId: 'Svetlanskaya' , building:'24В-24/3' , districtId: 'Dalzavod' ],
+            [streetId: 'Svetlanskaya' , building:'24/4-106' , districtId: 'Lugovaya' ]
+    ]
+
+
+    static void main(String[] args) {
+        Map initialSimpleData = [street: 'Svetlanskaya', building: '25']
+        testMapperSimple(initialSimpleData)
+
+        Map initialLiteralData = [street: 'Svetlanskaya', building: '3A']
+        //testMapperLiteral(initialLiteralData)
+
+        Map initialDashData = [street: 'Svetlanskaya', building: '24/1']
+        //testMapperDash(initialDashData)
+    }
+
+// SIMPLE ----------------------
+    static void testMapperSimple(Map initialSimpleData) {
+        mapperSimple.findAll {
+            it.streetId == initialSimpleData.street
+        }.each {
+            if (checkSimpleAccordance(initialSimpleData, it as Map)) {
+                println it.districtId
+            }
+        }
+    }
+
+    static Boolean checkSimpleAccordance(Map initialSimpleData, Map mapperValue) {
+        ( (initialSimpleData.building as Integer) <= (mapperValue.building.split('-')[1] as Integer)) &&
+                ( (initialSimpleData.building as Integer)  >= (mapperValue.building.split('-')[0] as Integer) )
+    }
+
+// END SIMPLE ----------------------
+
+
+    static void testMapperLiteral(Map initialLiteralData) {
+        println('test')
+    }
+
+    static void testMapperDash(Map initialDashData) {
+        println('test')
+    }
+
+
+}
