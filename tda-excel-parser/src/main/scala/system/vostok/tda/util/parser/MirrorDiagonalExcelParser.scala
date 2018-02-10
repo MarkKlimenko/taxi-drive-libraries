a0e0185d82b2fc5e392ae8fc7b257b3359bf5af8
MarkKlimenko <22773@bk.ru>
2018-02-10 13:29:28 +1000

    Minor refactoring

--- tda-excel-parser/src/main/scala/system/vostok/tda/util/parser/MirrorDiagonalExcelParser.scala
+++ tda-excel-parser/src/main/scala/system/vostok/tda/util/parser/MirrorDiagonalExcelParser.scala
@@ -25,28 +25,28 @@ class MirrorDiagonalExcelParser extends ExcelParser {
     var header = Iterable[String]()
     var result = ArrayBuffer[ParsedRow]()
 
-
     new SimpleTableExcelParser()
       .parse(file, sheetIndex)
       .getRidOfEmptyCells
       .zipWithIndex
-      .foreach { case (row, rowIndex) =>
+      .foreach { case (row, rowIndex) => processRow(row, rowIndex) }
 
-        if (rowIndex == 0) {
-          header = row.content
-        } else {
-          val from = row.content.toList.head
-          row.content
-            .zipWithIndex
-            .foreach { case (cell, cellIndex) =>
-              if (cellIndex != 0) {
-                result += new ParsedRow(
-                  rowType = SIMPLE,
-                  content = ArrayBuffer[String](from, header.toList(cellIndex - 1), cell))
-              }
+    def processRow(row: ParsedRow, rowIndex: Long): Unit = {
+      if (rowIndex == 0) {
+        header = row.content
+      } else {
+        row.content
+          .zipWithIndex
+          .foreach { case (cell, cellIndex) =>
+            if (cellIndex != 0) {
+              result += new ParsedRow(
+                rowType = SIMPLE,
+                content = ArrayBuffer[String](row.content.toList.head, header.toList(cellIndex - 1), cell))
             }
-        }
+          }
       }
+    }
+
     result
   }
 }
