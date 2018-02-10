bc8763f12af0098732965608a56eb9a162860197
MarkKlimenko <22773@bk.ru>
2018-02-10 13:10:21 +1000

    Implement all (before refactoring)

--- tda-excel-parser/src/main/scala/system/vostok/tda/util/parser/MirrorDiagonalExcelParser.scala
+++ tda-excel-parser/src/main/scala/system/vostok/tda/util/parser/MirrorDiagonalExcelParser.scala
@@ -3,7 +3,11 @@ package system.vostok.tda.util.parser
 import java.io.InputStream
 
 import system.vostok.tda.domain.ParsedRow
+import system.vostok.tda.util.IterableUtil
 import system.vostok.tda.util.constants.ParserType._
+import system.vostok.tda.util.constants.RowType.SIMPLE
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
   * Contain output in form
@@ -15,38 +19,34 @@ import system.vostok.tda.util.constants.ParserType._
 class MirrorDiagonalExcelParser extends ExcelParser {
   override val parserType: String = MIRROR_DIAGONAL
 
+  implicit def bindToToIterable(iterable: Iterable[ParsedRow]): IterableUtil = new IterableUtil(iterable)
+
   override def parse(file: InputStream, sheetIndex: Integer): Iterable[ParsedRow] = {
-    new SimpleTableExcelParser().parse(file, sheetIndex)
-
-  }
-
+    var header = Iterable[String]()
+    var result = ArrayBuffer[ParsedRow]()
 
 
-  /*static List mirrorDiagonalExcelToList(Object file, Integer sheetIndex) {
-    List header = []
-    List result = []
+    new SimpleTableExcelParser()
+      .parse(file, sheetIndex)
+      .getRidOfEmptyCells
+      .zipWithIndex
+      .foreach { case (row, rowIndex) =>
 
-    plainHeaderExcelToList(file, sheetIndex)
-    .with { getRidOfEmptyCells(it) }
-      .eachWithIndex { row, rowIndex ->
         if (rowIndex == 0) {
-          header = row
+          header = row.content
         } else {
-          String from = row.first()
-          row.eachWithIndex { cell, cellIndex ->
-            if (cellIndex != 0) {
-              result << [from, header[cellIndex - 1], cell]
+          val from = row.content.toList.head
+          row.content
+            .zipWithIndex
+            .foreach { case (cell, cellIndex) =>
+              if (cellIndex != 0) {
+                result += new ParsedRow(
+                  rowType = SIMPLE,
+                  content = ArrayBuffer[String](from, header.toList(cellIndex - 1), cell))
+              }
             }
-          }
         }
       }
     result
   }
-
-  static List getRidOfEmptyCells(List table) {
-    table.findAll { it }
-      .collect { it.findAll { it } }
-      .findAll { it }
-  }*/
-
 }
